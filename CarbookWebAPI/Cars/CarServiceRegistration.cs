1974ebc228351e7187b941f48663f74a34a61b83
JoachimChrapek <33358648+JoachimChrapek@users.noreply.github.com>
2023-12-05 21:19:40 +0100

    Added Scrutor * Added car service decorator for measuring request duration

--- CarbookWebAPI/Cars/CarServiceRegistration.cs
+++ CarbookWebAPI/Cars/CarServiceRegistration.cs
@@ -6,7 +6,15 @@ public static class CarServiceRegistration
 {
     public static IServiceCollection AddCarServices(this IServiceCollection services)
     {
+        // services.TryAddScoped<CarService>();
+        // services.TryAddScoped<ICarService>(provider =>
+        // {
+        //     return new LoggedCarService(provider.GetRequiredService<CarService>(), provider.GetRequiredService<ILogger<ICarService>>());
+        // });
+        
         services.TryAddScoped<ICarService, CarService>();
+        services.TryDecorate<ICarService, LoggedCarService>();
+        
         services.TryAddScoped<ICarProvider, CarProvider>();
         services.TryAddScoped<ICarModelProvider, CarModelProvider>();
         services.TryAddScoped<ICarMileageProvider, CarMileageProvider>();
---
+++ CarbookWebAPI/Cars/LoggedCarService.cs
@@ -0,0 +1,27 @@
+﻿using CarbookWebAPI.Logging;
+
+namespace CarbookWebAPI.Cars;
+
+public class LoggedCarService : ICarService
+{
+    private readonly ICarService _carService;
+    private readonly ILogger<ICarService> _logger;
+
+    public LoggedCarService(ICarService carService, ILogger<ICarService> logger)
+    {
+        _carService = carService;
+        _logger = logger;
+    }
+
+    public async Task<Car> GetRandomCarAsync()
+    {
+        using IDisposable _ = _logger.TimedOperation(LogLevel.Information, $"{nameof(GetRandomCarAsync)}");
+        return await _carService.GetRandomCarAsync();
+    }
+
+    public async Task<IEnumerable<Car>> GetRandomCarsCollectionAsync(int count)
+    {
+        using IDisposable _ = _logger.TimedOperation(LogLevel.Information, $"{nameof(GetRandomCarsCollectionAsync)}");
+        return await _carService.GetRandomCarsCollectionAsync(count);
+    }
+}
