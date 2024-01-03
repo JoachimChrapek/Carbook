f9ed9b9b0cf9b8c6440ed4ff5fe14adb89ec720e
JoachimChrapek <33358648+JoachimChrapek@users.noreply.github.com>
2024-01-03 16:43:38 +0100

    Reorganized project structure - added src and test folders

---
+++ src/Test.ConsoleApp/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace Test.ConsoleApp;
+
+public class Calculator
+{
+    public int Square(int num)
+    {
+        return num * num;
+    }
+
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public int Multiply(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public int Subtract(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    public float Division(float num1, float num2)
+    {
+        return num1 / num2;
+    }
+}
